466630ace769bbd15e2276ebd4126faf88af7927
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2024-09-16 05:35:20 -0300

    deploy vercel?

--- src/server.js
+++ src/server.js
@@ -5,20 +5,19 @@ import cors from 'cors';
 import './config/db.js';
 
 import { logger } from './middlewares/logger.js';
-
 import indexRouter from './router/indexRouter.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 const app = express();
+
 app.use(logger);
 app.use(cors());
 app.use(express.json());
-app.listen(PORT , () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
-});
-app.use('/', indexRouter);
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}); 
 
+app.use('/', indexRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+export default app;
