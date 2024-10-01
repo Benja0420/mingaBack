ccd1a3802b93e8472597b084d10ff7de4857bc78
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2024-10-01 16:59:37 -0300

    asdasd

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(logger);
 app.use(cors());
 app.use(express.json());
 //descomentar la linea de abajo para que funcione en local
-app.listen(process.env.PORT , () => {console.log(`Server is running on port ${process.env.PORT}`);});
+//app.listen(process.env.PORT , () => {console.log(`Server is running on port ${process.env.PORT}`);});
 app.use('/', indexRouter);
 app.get('/', (req, res) => {
   res.send('Hello World!');
