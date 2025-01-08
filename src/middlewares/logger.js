4456aed3918166b7e530d8e716e7a573c2c2c42d
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2025-01-07 23:13:49 -0300

    logger

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,4 +1,4 @@
 export function logger(req, res, next) {
-  console.log("Metodo: " + req.method +" Path: " + req.url + " Fecha: "+ new Date()+ " IP: "+ req.ip + " statusCode: "+ res.statusCode + " operating system: "+ req.get('User-Agent'));
+  console.log("Metodo: " + req.method +" Path: " + req.url + " Fecha: "+ new Date()+" statusCode: "+ res.statusCode);
   next();
 }
