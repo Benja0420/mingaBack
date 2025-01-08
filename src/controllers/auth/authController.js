fbebcd0cc68d4f65ab6dd5512d7319c73bd235fa
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2025-01-08 10:50:35 -0300

    asd

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -72,17 +72,17 @@ async function login(req, res) {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
-      return res.status(400).json({ message: "Correo electrónico no existe" });
+      return res.status(400).json({ message: "Email not found" });
     }
     if (bcrypt.compareSync(req.body.password, user.password)) {
       const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
       res.status(200).json({ token });
     } else {
-      return res.status(400).json({ message: "Correo electrónico o contraseña inválidos" });
+      return res.status(400).json({ message: "Email or password invalid" });
     }
   } catch (error) {
-    console.error("Error durante el inicio de sesión:", error);
-    return res.status(500).json({ message: "Error del servidor" });
+    console.error("Error:", error);
+    return res.status(500).json({ message: "Server error" });
   }
 }
 
