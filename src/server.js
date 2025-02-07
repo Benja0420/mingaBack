107b20e6a817acd334fd8a55a181c83a719a242b
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2025-02-06 22:03:28 -0300

    port

--- src/server.js
+++ src/server.js
@@ -9,15 +9,15 @@ import indexRouter from "./router/indexRouter.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+/* const PORT = process.env.PORT || 5000; */
 
 app.use(logger);
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json());
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`\x1b[4;94mhttp://localhost:${PORT}\x1b[0m`);
-});
+}); */
 app.use("/", indexRouter);
 app.get("/", (req, res) => {
   res.send("ok ✅🟢");
